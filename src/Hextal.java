9480b15ff29a9c4fb7d3099f2cd85ceb2cf1c18e
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2019-05-29 10:29:18 +0200

    Stringen kan nu läsas in. Har den 0x, skriver den ut hela stringen

--- src/Hextal.java
+++ src/Hextal.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Hextal {
     public static void main(String[] args) {
-
+        System.out.println("Skriv in hexadecimala tal här (åtminstone ett): ");
+        Scanner sn = new Scanner(System.in);
+        String hextal = sn.nextLine();
+        if (hextal.contains("0x")) {
+            System.out.println(hextal);
+        } else {
+            System.exit(0);
+        }
     }
 }
