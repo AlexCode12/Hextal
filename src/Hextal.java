07e1c51f1c97d449bd15b9912c651b704731e3d3
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2019-06-05 08:46:22 +0200

    Skriver ut talet i hexadecimalt format, ish

--- src/Hextal.java
+++ src/Hextal.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Hextal {
@@ -5,10 +6,25 @@ public class Hextal {
         System.out.println("Skriv in hexadecimala tal här (åtminstone ett): ");
         Scanner sn = new Scanner(System.in);
         String hextal = sn.nextLine();
-        if (hextal.contains("0x")) {
-            System.out.println(hextal);
-        } else {
-            System.exit(0);
+        int length = hextal.length();
+        String ja = "";
+        int [] numbers = {0,1,2,3,4,5,6,7,8,9};
+        for (int i = 0; i<length; i++) {
+            char s = hextal.charAt(i);
+            String g = Character.toString(s);
+            switch (Character.toString(s)) {
+                case "0": case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9": case "x": case "a": case "b": case "c": case "d": case "e": case "f": case "A": case "B": case "C": case "D": case "E": case "F":
+            ja += s;
+            break;
+            }
+            ArrayList<String> ar = new ArrayList<String>();
+            String[] arrSplit = ja.split("0x");
+            for (int x=1; x < arrSplit.length; x++)
+            {
+                String thenumbers = arrSplit[x];
+                System.out.println(thenumbers);
+                System.out.println(""+Integer.valueOf(thenumbers,16));
+            }
         }
     }
 }
